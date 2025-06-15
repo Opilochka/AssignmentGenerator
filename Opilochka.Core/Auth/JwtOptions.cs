bd523e1153a128a418b657f99aea0cf81e139a3b
opilochka <olavalieva67637@gmail.com>
2025-06-15 16:21:00 +0300

    fix

--- Opilochka.Core/Auth/JwtOptions.cs
+++ Opilochka.Core/Auth/JwtOptions.cs
@@ -3,7 +3,7 @@
     public class JwtOptions
     {
         public string SecretKey { get; set; } = string.Empty;
-        public string Issuer { get; set; } = string.Empty;
-        public string Audience { get; set; } = string.Empty;
+        public string Issuer { get; set; } = "Opilochka.API";
+        public string Audience { get; set; } = "Opilochka.Web";
     }
 }
